7c5639bef7707c3388478630e5a96793c995225e
Amelia Winger <ameliawinger@Amelias-MacBook-Pro.local>
2025-06-19 12:02:42 -0500

    first run

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState, useRef } from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div id="homepage-name-container">
+        <div className="home-name-wrapper">
+          <div className="home-name">
+            <div>Test</div>
+            <div>Test</div>
+          </div>
+          <div style={{ backgroundColor: "purple" }}>Test</div>
+        </div>
+        <div style={{ backgroundColor: "cyan" }}></div>
+      </div>
+
+      <div>Button grid eventually</div>
+    </div>
+  );
+};
+
+export default Home;
